e5c20e31ef37f20148560cce778aba935aa9e093
ahmed00012 <52130117+ahmed00012@users.noreply.github.com>
2019-07-04 21:15:46 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/nh/books/MainActivity.java
+++ app/src/main/java/com/example/nh/books/MainActivity.java
@@ -6,12 +6,12 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView listView;
+    RecyclerView r;
     ArrayList<book> book1;
 
     @Override
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
         book1.add(new book(R.drawable.spirits, 3.5f,"The House of the Spirits ","by Isabel Allende"));
         book1.add(new book(R.drawable.sword, 2.5f,"The Sword Thief  "," by Peter Lerangis"));
         book1.add(new book(R.drawable.solitude, 2f,"One Hundred Years of Solitude ","by Gabriel García Márquez"));
-        listView=findViewById(R.id.list1);
+        r =findViewById(R.id.list1);
         bookadapter adapter=new bookadapter(book1,MainActivity.this);
-        listView.setLayoutManager(new LinearLayoutManager(this));
-       listView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        listView.setAdapter(adapter);
+        r.setLayoutManager(new LinearLayoutManager(this));
+        r.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        r.setAdapter(adapter);
 
 
     }
